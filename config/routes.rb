1e72b47c0d6ce3886308533069db10a0a5259105
Samar Ramireddy <samar.ramireddy@ggktech.com>
2020-12-26 20:47:48 +0530

    Added support for English and Spanish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: "contacts#show"
-  get '/contact', to: "contacts#show"
-  post '/contact_us', to: "contacts#contact_us"
-  match '*a', to: "application#page_not_found", via: :get
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root to: "contacts#show"
+    get '/contact', to: "contacts#show"
+    post '/contact_us', to: "contacts#contact_us"
+    match '*a', to: "application#page_not_found", via: :get
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
