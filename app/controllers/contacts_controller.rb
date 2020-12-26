e2bc1e816b02ae80df4c661a3be8a5f2ebbd9371
Samar Ramireddy <samar.ramireddy@ggktech.com>
2020-12-26 14:19:30 +0530

    Save contacts to database with validations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,22 @@
 class ContactsController < ApplicationController
+  skip_before_action :verify_authenticity_token 
   def show
   end
 
   def contact_us
+    @contact = Contact.new(permitted_params)
+    respond_to do |format|   
+      if @contact.save
+        format.html { redirect_to contact_url, notice: 'Contact was successfully created.' }   
+      else
+        flash.keep[:errors] = @contact.errors.full_messages
+        format.html { redirect_to contact_url } 
+      end 
+    end
   end
+
+  private
+    def permitted_params
+      params.permit(:first_name, :last_name, :email, :phone_number, :message)
+    end
 end
